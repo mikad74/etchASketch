dbeec354999cb55039931e06f91943e7b58611d7
mikad74 <mika.doorenbosch@gmail.com>
2022-07-07 13:14:24 +0200

    Added the grid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("Hello, World!")
+
+
+function createGrid() {
+  const gridContainer = document.querySelector(".grid-container")
+  for (i = 0; i < 16; i++) {
+    const row = document.createElement("div")
+    row.classList.add("row")
+    for (j = 0; j < 16; j++) {
+      const column = document.createElement("div")
+      column.classList.add("column")
+      row.appendChild(column)
+    }
+    gridContainer.appendChild(row)
+  }
+}
+
+createGrid()
