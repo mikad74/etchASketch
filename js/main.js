28c6cc0c05cd3b77f36544498d800ef5b591f1a1
mikad74 <mika.doorenbosch@gmail.com>
2022-07-07 14:11:50 +0200

    working etch a sketch

--- js/main.js
+++ js/main.js
@@ -1,18 +1,49 @@
 console.log("Hello, World!")
 
+clicking = false
 
-function createGrid() {
+function colorSquare(click) {
+  if (clicking) this.classList.add("colored")
+}
+
+function createGrid(gridSize) {
   const gridContainer = document.querySelector(".grid-container")
-  for (i = 0; i < 16; i++) {
+  gridContainer.innerHTML = ""
+  for (i = 0; i < gridSize; i++) {
     const row = document.createElement("div")
     row.classList.add("row")
-    for (j = 0; j < 16; j++) {
+    for (j = 0; j < gridSize; j++) {
       const column = document.createElement("div")
       column.classList.add("column")
+      column.setAttribute("draggable", false)
       row.appendChild(column)
+      column.addEventListener('mouseover', colorSquare)
     }
     gridContainer.appendChild(row)
   }
 }
 
-createGrid()
+function changeGridSize() {
+  const gridSize = prompt("What size do you want the grid to be?")
+  createGrid(gridSize)
+
+  console.log("changing grid size")
+}
+
+document.addEventListener("mousedown", mouseDown)
+document.addEventListener("mouseup", mouseUp)
+
+const sizeButton = document.querySelector(".size-button").firstElementChild
+console.log(sizeButton)
+sizeButton.addEventListener("click", changeGridSize)
+
+function mouseDown() {
+  clicking = true
+}
+
+function mouseUp() {
+  clicking = false
+}
+
+createGrid(16)
+
